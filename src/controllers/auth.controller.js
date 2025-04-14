e680127ca3fb9220ea485a9bed2df1c18a10cd8e
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-14 18:41:20 +0000

    Define functions for register and sign in.

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,69 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// @desc    Register a new user
+exports.register = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    // validation
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({ success: false, message: "All fields are required!" });
+    }
+    // check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ success: false, message: "User already exists!" });
+    }
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // create new user
+    const newUser = new User({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hashedPassword
+    })
+    // save new user to db
+    const savedUser = await newUser.save();
+    res.status(201).json({ success: true, message: "User registered successfully!", user: savedUser });
+  } catch (err) {
+    console.error("Error in registering user: ", err);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+// @desc    sign in user
+exports.signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // validation
+    if (!email || !password) {
+      return res.status(400).json({ success: false, message: "All fields are required!" });
+    };
+    /// check if user exists
+    const existingUser = await User.findOne({ email }).select("+password");
+    if (!existingUser) {
+      return res.status(400).json({ success: false, message: "User does not exist!" });
+    }
+    // check password
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ success: false, message: "User does not exist!" });
+    }
+    // generate token
+    const accessToken = jwt.sign({ id: existingUser._id, email: existingUser.email }, process.env.PRIVATE_KEY, { expiresIn : "1d" })
+    // remove password from user object
+    const { password: _ , ...userObject } = existingUser._doc; 
+    const user = { ...userObject };
+    // ALT 
+    // const userObject = existingUser.toObject();
+    // delete userObject.password;
+
+    res.status(200).json({ success: true, message: "User signed in successfully!", user: user, accessToken });
+  } catch (err) {
+    console.log("Error in signing in user: ", err);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+} 
