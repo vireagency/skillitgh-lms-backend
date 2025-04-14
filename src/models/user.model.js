0ca4247e4cd9bb0a941279d2bf3eb375497cc8aa
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-14 18:39:11 +0000

    Created user schema.

---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  last_name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    select: false
+  }
+})
