541255d071897e877777b65424351e61fe673a87
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-22 20:20:03 +0000

    Add course routes

---
+++ src/routes/course.route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const courseController = require('../controllers/course.controller');
+
+const { auth } = require('../middlewares/auth.middleware');
+const { role } = require('../middlewares/role.middleware');
+
+// Route to get all courses
+router.get('/courses/', auth, courseController.getCourses);
+
+// Route to get a course by ID
+router.get('/courses/:courseId', auth, courseController.getCourseById);
+
+// Route to register for a course
+router.post('/courses/:courseId/register', auth, courseController.registerForCourse);
+
+// Route to create a new course (admin only)
+router.post('/courses/', auth, role, courseController.createCourse);
