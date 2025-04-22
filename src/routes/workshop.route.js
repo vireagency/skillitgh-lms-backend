85b30c74a991ed703292ab63eb137963aa10a8a6
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-22 20:20:30 +0000

    Change workshop routes

--- src/routes/workshop.route.js
+++ src/routes/workshop.route.js
@@ -14,30 +14,30 @@ const {
  * @access     Public
  */
 
-router.get('/upcoming', getUpcomingWorkshops);
+router.get('/workshops/upcoming', getUpcomingWorkshops);
 
 /** @route     GET api/workshops/previous
  * @desc       Get all previous workshops
  * @access     Public
  */
-router.get('/previous', getPreviousWorkshops);
+router.get('/workshops/previous', getPreviousWorkshops);
 
 /** @route     GET api/workshops/:workshopId
  * @desc       Get workshop by ID
  * @access     Public
  */
-router.get('/:workshopId', getWorkshopById);
+router.get('/workshops/:workshopId', getWorkshopById);
 
 /** @route     POST api/workshops/register
  * @desc       Register for a workshop
  * @access     Private
  */
-router.post('/register/:workshopId', registerForWorkshop);
+router.post('/workshops/:workshopId/register',auth, registerForWorkshop);
 
 /** @route     POST api/workshops/
  * @desc       Create a new workshop
  * @access     Private
  */
-router.post('/', createWorkshop);
+router.post('/workshops/', createWorkshop);
 
 module.exports = router;
