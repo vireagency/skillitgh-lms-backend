ad1b8c1d9fa0d338e3f41a063de310486e81b6ef
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-22 20:03:15 +0000

    Create a model for course

---
+++ src/models/course.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  instructor: {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    }
+  },
+  duration: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  externalFormLink: String,
+  enrolledUsers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+});
+
+module.exports = mongoose.model('Course', courseSchema);
