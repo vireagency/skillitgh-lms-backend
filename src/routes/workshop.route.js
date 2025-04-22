1c5d581661763cee2e079aeb7daefe1b8c1bc295
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-22 07:24:06 +0000

    Add route descriptions

--- src/routes/workshop.route.js
+++ src/routes/workshop.route.js
@@ -1,18 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const { auth } = require('../middlewares/auth.middleware');
-const { 
+const {
    getUpcomingWorkshops,
-   getPreviousWorkshops, 
-   getWorkshopById, 
+   getPreviousWorkshops,
+   getWorkshopById,
    registerForWorkshop,
-   createWorkshop 
+   createWorkshop
 } = require('../controllers/workshop.controller');
 
+/** @route     GET api/workshops/upcoming 
+ * @desc       Get all upcoming workshops
+ * @access     Public
+ */
+
 router.get('/upcoming', getUpcomingWorkshops);
+
+/** @route     GET api/workshops/previous
+ * @desc       Get all previous workshops
+ * @access     Public
+ */
 router.get('/previous', getPreviousWorkshops);
+
+/** @route     GET api/workshops/:workshopId
+ * @desc       Get workshop by ID
+ * @access     Public
+ */
 router.get('/:workshopId', getWorkshopById);
-router.post('/register', auth, registerForWorkshop);
-router.post('/upload', createWorkshop);
+
+/** @route     POST api/workshops/register
+ * @desc       Register for a workshop
+ * @access     Private
+ */
+router.post('/register/:workshopId', registerForWorkshop);
+
+/** @route     POST api/workshops/
+ * @desc       Create a new workshop
+ * @access     Private
+ */
+router.post('/', createWorkshop);
 
 module.exports = router;
