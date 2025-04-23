1da0a2424cdb2ff92593fb6473ed848338eb9668
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-23 07:37:22 +0000

    Add description and role middleware to the routes.

--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -3,18 +3,30 @@ const router = express.Router();
 const courseController = require('../controllers/course.controller');
 
 const { auth } = require('../middlewares/auth.middleware');
-//const { role } = require('../middlewares/role.middleware');
+const { authorizeRole } = require('../middlewares/role.middleware');
 
-// Route to get all courses
+/** @route    GET api/v1/courses/
+ * @desc      Fetch all courses 
+ * @access    Public
+*/
 router.get('/courses/', auth, courseController.getCourses);
 
-// Route to get a course by ID
+/** @route    GET api/v1/courses/:courseId/
+ * @desc      Fetch a course by ID
+ * @access    Public
+ */
 router.get('/courses/:courseId', auth, courseController.getCourseById);
 
-// Route to register for a course
+/** @route    POST api/v1/courses/:courseId/register/ 
+ * @desc      Register for a course
+ * @access    Private
+ */
 router.post('/courses/:courseId/register', auth, courseController.registerForCourse);
 
-// Route to create a new course (admin only)
-router.post('/courses/', auth, courseController.createCourse);
+/** @route    POST api/v1/courses/ 
+ * @desc      Create a new course
+ * @access    Private (admin only)
+ */
+router.post('/courses/', auth, authorizeRole('admin'), courseController.createCourse);
 
 module.exports = router;
