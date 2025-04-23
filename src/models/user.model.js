da75a9bd05ab8ed7c254b926c933cb284124b9e4
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-23 07:37:47 +0000

    Add role to the user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -23,6 +23,11 @@ const userSchema = new mongoose.Schema({
     required: true,
     trim: true,
     select: false
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user'
   }
 }, { timestamps: true })
 
