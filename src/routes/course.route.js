2ca6a40321b606506b7875e5ee8da4ec07e8a955
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-25 10:32:10 +0000

    Define the routes for dashboard operations

--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -9,24 +9,43 @@ const { authorizeRole } = require('../middlewares/role.middleware');
  * @desc      Fetch all courses 
  * @access    Public
 */
-router.get('/courses/', auth, courseController.getCourses);
+router.get('/courses', auth, courseController.getCourses);
 
 /** @route    GET api/v1/courses/:courseId/
  * @desc      Fetch a course by ID
  * @access    Public
  */
-router.get('/courses/:courseId', auth, courseController.getCourseById);
+router.get('/:courseId', auth, courseController.getCourseById);
 
 /** @route    POST api/v1/courses/:courseId/register/ 
  * @desc      Register for a course
  * @access    Private
  */
-router.post('/courses/:courseId/register', auth, courseController.registerForCourse);
+router.post('/:courses/register', auth, courseController.registerForCourse);
 
 /** @route    POST api/v1/courses/ 
  * @desc      Create a new course
  * @access    Private (admin only)
  */
-router.post('/courses/', auth, authorizeRole('admin'), courseController.createCourse);
+router.post('/courses', auth, authorizeRole('admin'), courseController.createCourse);
+
+/** @route    GET api/v1/dashboard/registeredCourses
+ * @desc      GET all registered courses for a user
+ * @access    Private
+ */
+router.get('/dashboard/registeredCourses', auth, courseController.getRegisteredCourses);
+
+/** @route    GET api/v1/dashboard/registeredUsers
+ * @desc      GET all registered users for a course
+ * @access    Private (admin only)
+ */
+router.get('/dashboard/:courseId/registeredUsers', auth, authorizeRole('admin'), courseController.getRegisteredUsers);
+
+
+/** @route    GET api/v1/dashboard/otherCourses
+ * @desc      GET all other courses
+ * @access    Private 
+ */
+router.get('/dashboard/otherCourses', auth, courseController.getOtherCourses);
 
 module.exports = router;
