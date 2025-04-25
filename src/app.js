0aa9531f6ad8c7a1062afd9f6f392d61c1f25469
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-25 10:37:11 +0000

    Remove mongodb connection from this file.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const authRoutes = require("./routes/auth.route");
 const workshopRoutes = require("./routes/workshop.route");
 const courseRoutes = require("./routes/course.route");
+const { connectDB } = require("./config/db");
 
 dotenv.config();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -20,10 +21,5 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use("/api/v1/", authRoutes, workshopRoutes, courseRoutes);
-
-// MongoDB connection
-mongoose.connect(process.env.DB_URI)
-  .then(() => console.log("MongoDB connected successfully!"))
-  .catch(err => console.log("MongoDB connection failed!", err))
 
 module.exports = app;
