7dc773e0b222ef73ee1b1ea50d7a067dcf34faec
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-30 14:10:22 +0000

    Add an image to the course schema

--- src/models/course.model.js
+++ src/models/course.model.js
@@ -3,11 +3,18 @@ const mongoose = require('mongoose');
 const courseSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
+    trim: true  
   },
   description: {
     type: String,
+    required: true,
+    trim: true
   },
+  courseImage: {
+    type: String,
+    trim: true
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Course', courseSchema);
