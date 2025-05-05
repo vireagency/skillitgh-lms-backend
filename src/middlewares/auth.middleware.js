b0f463affd908e953bba77fe5f129afe7ac296f0
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-05 00:36:41 +0000

    Enhance error handling in auth middleware.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,15 +6,21 @@ exports.auth = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (!token) {
-      return res.status(401).json({ success: false, message: "No token provided. Access denied. Please log in." });
+      return res.status(401).json({ success: false, message: "Access denied. Please log in." });
     }
     // verify token
-    const decoded = jwt.verify(token, process.env.PRIVATE_KEY);
-    // Attach user info to the request
-    req.user = await User.findById(decoded.id);
+    jwt.verify(token, process.env.PRIVATE_KEY, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ success: false, message: "Access denied! Please log in again." })
+      }
+      req.user = User.findById(decoded.id);
+      console.log("Decoded token:", decoded); // Log the decoded token for debugging
+    });
     if (!req.user) {
-      return res.status(401).json({ success: false, message: "Invalid token. Access denied." });
+      return res.status(401).json({ success: false, message: "Access denied! Please log in again." });
     }
+    console.log("User found:", req.user); // Log the user for debugging
+    // Proceed to the next middleware or route handler
     next();
   } catch (err) {
     console.error("Invalid or expired token", err.message);
