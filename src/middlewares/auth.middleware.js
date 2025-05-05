0cc9654488d8c64a314e8710cbe1df66c1e5c78d
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-05 11:13:13 +0000

    Add a callback inside jwt verification to handle errors.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -13,15 +13,14 @@ exports.auth = async (req, res, next) => {
       if (err) {
         return res.status(401).json({ success: false, message: "Access denied! Please log in again." })
       }
-      req.user = User.findById(decoded.id);
+      req.user = { userId: decoded.id, role: decoded.role }; // Attach user ID and role to request object
+      if (!req.user) {
+        return res.status(401).json({ success: false, message: "Unauthorized: Please log in again." });
+      }
       console.log("Decoded token:", decoded); // Log the decoded token for debugging
+      console.log("User found:", req.user); // Log the user for debugging
+      next();
     });
-    if (!req.user) {
-      return res.status(401).json({ success: false, message: "Access denied! Please log in again." });
-    }
-    console.log("User found:", req.user); // Log the user for debugging
-    // Proceed to the next middleware or route handler
-    next();
   } catch (err) {
     console.error("Invalid or expired token", err.message);
     res.status(500).json({ success: false, message: "Internal Server Error" });
