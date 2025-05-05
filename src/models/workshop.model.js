a729530bf003377b9547eec9e1b22093a76a0136
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-05 16:57:03 +0000

    Add a default image to the workshop

--- src/models/workshop.model.js
+++ src/models/workshop.model.js
@@ -13,7 +13,7 @@ workshopSchema = new mongoose.Schema({
   workshopImage: {
     type: String,
     trim: true,
-    default: "https://asset.cloudinary.com/dc7waspih/5298eb1df4079707814dcf1b435a0af7"
+    default: "https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   date: {
     type: Date,
