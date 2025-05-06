26d49b9590cbb1627fe60aeaea95101224423ac2
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-06 03:47:20 +0000

    Make docs for course routes.

--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -31,14 +31,18 @@ router.get('/:courseId', auth, courseController.getCourseById);
  *           schema:
  *             type: object
  *             properties:
- *               courseId:
+ *               courseTitle:
  *                 type: string
- *                 description: The ID of the course to register for.
- *                 example: "1234567890abcdef12345678"
- *               reason:
+ *                 description: The title of a course.
+ *                 example: "Graphic Design"
+ *               messageBody:
  *                 type: string
  *                 description: The reason for registering for the course.
  *                 example: "I want to learn more about this topic."
+ *               courseImage:
+ *                 type: string
+ *                 description: The URL of the course image.
+ *                 example: "https://example.com/course-image.jpg"
  *     responses:
  *       200:
  *         description: Successfully registered for the course.
@@ -53,8 +57,20 @@ router.get('/:courseId', auth, courseController.getCourseById);
  *                 success: 
  *                   type: boolean
  *                   example: true
+ *                 course:
+ *                  type: object
+ *                  properties:
+ *                   _id:
+ *                     type: string
+ *                     example: "1234567890abcdef12345678"
+ *                   enrolledUser:
+ *                     type: string
+ *                     example: "1234567890abcdef12345678"
+ *                   messageBody:
+ *                     type: string
+ *                     example: "I want to learn more about this topic."
  *       400:
- *          description: Course Id is required or You have already registered for this course.
+ *          description: Course title is required or You have already registered for this course.
  *          content: 
  *            application/json:
  *              schema:
@@ -62,7 +78,7 @@ router.get('/:courseId', auth, courseController.getCourseById);
  *                properties:
  *                  message:
  *                    type: string
- *                    example: "Course Id is required or You have already registered for this course."
+ *                    example: "Course title is required or You have already registered for this course."
  *                  success:
  *                    type: boolean
  *                    example: true              
@@ -96,6 +112,86 @@ router.post('/courses/register', auth, courseController.registerForCourse);
 router.post('/courses', auth, authorizeRole('admin'), courseController.createCourse);
 
 /** 
+ * @swagger
+ * /api/v1/courses/dashboard/registeredCourses:
+ *   get:
+ *     summary: Get all registered courses for a user
+ *     description: This endpoint allows a user to fetch all courses they have registered for.
+ *     tags: ["Courses"]
+ *     responses:
+ *       200:
+ *         description: Successfully fetched all registered courses.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Successfully fetched all registered courses."
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 courses:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       _id:
+ *                         type: string
+ *                         example: "1234567890abcdef12345678"
+ *                       courseTitle:
+ *                         type: string
+ *                         example: "Graphic Design"
+ *                       messageBody:
+ *                         type: string
+ *                         example: "I want to learn more about this topic."
+ *                       courseImage:
+ *                         type: string
+ *                         example: "https://example.com/course-image.jpg"
+ * 
+ *       401:
+ *         description: Unauthorized:Please Login.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Unauthorized: Please Login."
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *       404: 
+ *         description: No registered course found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "No registered course found."
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *       500:
+ *          description: Internal Server Error
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: "Internal Server Error"
+ *                  success:
+ *                    type: boolean
+ *                    example: false
+ * 
+ * 
+ * 
  * @route    GET api/v1/dashboard/registeredCourses
  * @desc      GET all registered courses for a user
  * @access    Private
@@ -110,14 +206,178 @@ router.get('/dashboard/registeredCourses', auth, courseController.getRegisteredC
 router.get('/dashboard/:courseId/registeredUsers', auth, authorizeRole('admin'), courseController.getRegisteredUsers);
 
 
-/** 
+/**
+ * @swagger
+ * /api/v1/courses/dashboard/otherCourses:
+ *   get:
+ *     summary: Get all other courses for a user
+ *     description: This endpoint allows a user to fetch all other courses they can register.
+ *     tags: ["Courses"]
+ *     responses:
+ *       200:
+ *         description: Successfully fetched all other courses.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Successfully fetched all other courses."
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 courses:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       _id:
+ *                         type: string
+ *                         example: "1234567890abcdef12345678"
+ *                       courseTitle:
+ *                         type: string
+ *                         example: "Graphic Design"
+ *                       messageBody:
+ *                         type: string
+ *                         example: "I want to learn more about this topic."
+ *                       courseImage:
+ *                         type: string
+ *                         example: "https://example.com/course-image.jpg"
+ * 
+ *       401:
+ *         description: Unauthorized:Please Login.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Unauthorized: Please Login."
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *       404: 
+ *         description: No registered course found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "No registered course found."
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *       500:
+ *          description: Internal Server Error
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: "Internal Server Error"
+ *                  success:
+ *                    type: boolean
+ *                    example: false 
+ * 
  * @route    GET api/v1/dashboard/otherCourses
  * @desc      GET all other courses
  * @access    Private 
  */
 router.get('/dashboard/otherCourses', auth, courseController.getOtherCourses);
 
-/** 
+/**
+ * @swagger
+ * /api/v1/courses/dashboard/{courseId}/register:
+ *   get:
+ *     summary: Register for other courses
+ *     description: This endpoint allows a user to register for other courses.
+ *     parameters:
+ *       - in: path
+ *         name: courseId
+ *         required: true
+ *         description: The ID of the course to register for.
+ *         schema:
+ *           type: string
+ *           example: "1234567890abcdef12345678"
+ *     tags: ["Courses"]
+ *     responses:
+ *       200:
+ *         description: Successfully registered for this course.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Successfully registered for this course."
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 courses:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       _id:
+ *                         type: string
+ *                         example: "1234567890abcdef12345678"
+ *                       courseTitle:
+ *                         type: string
+ *                         example: "Graphic Design"
+ *                       messageBody:
+ *                         type: string
+ *                         example: "I want to learn more about this topic."
+ *                       courseImage:
+ *                         type: string
+ *                         example: "https://example.com/course-image.jpg"
+ * 
+ *       401:
+ *         description: Unauthorized:Please Login.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Unauthorized: Please Login."
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *       404: 
+ *         description: No registered course found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "No registered course found."
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *       500:
+ *          description: Internal Server Error
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: "Internal Server Error"
+ *                  success:
+ *                    type: boolean
+ *                    example: false
+ *  
  * @route   GET api/v1/dashboard/{courseId}/registier 
  * @desc     Register for other courses
  * @access   Private
