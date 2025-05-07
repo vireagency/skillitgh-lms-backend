a38e5802b937153ca0bf6a9934cc7d484b4df861
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-07 14:13:33 +0000

    Remove isRegistered from userSchema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,10 +46,6 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false
   },
-  workshopRegistered: {
-    type: Boolean,
-    default: false
-  },
 }, { timestamps: true })
 
 module.exports = mongoose.model('User', userSchema);      
