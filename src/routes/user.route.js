9963bcd5b6ffb9dcdfbcb92c76c5ed06ce6c005e
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-13 18:15:18 +0000

    Define routes  user crud operations

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const { getUserProfile, updateUserProfile } = require('../controllers/user.controller');
+const { getUserProfile, updateUserProfile, deleteUserProfile, getAllUsers } = require('../controllers/user.controller');
 const { auth } = require('../middlewares/auth.middleware');
+const { authorizeRole } = require('../middlewares/role.middleware');
 
+/**
+ * @route    GET api/v1/dashboard/profile
+ * @desc     Get user profile
+ * @access   Private
+ */
 router.get('/dashboard/profile', auth, getUserProfile);
+
+/**
+ * @route    PUT api/v1/dashboard/profile
+ * @desc     Update user profile
+ * @access   Private
+ */
 router.put('/dashboard/profile', auth, updateUserProfile);
+
+/** @  
+ * @route    DELETE api/v1/dashboard/profile
+ * @desc     Delete user profile
+ * @access   Private
+ */
+router.delete('/dashboard/profile', auth, authorizeRole('admin'), deleteUserProfile)
+
+/**
+ * @route    GET api/v1/dashboard/users
+ * @desc     Get all users on the platform
+ * @access   Private
+ */
+router.get('/dashboard/users', auth, authorizeRole('admin'), getAllUsers);
 
 module.exports = router;
