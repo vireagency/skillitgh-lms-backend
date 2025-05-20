9607ffc25df5483f6dd24d8cd293b34ff6665868
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-20 16:58:03 +0000

    Improve the multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -6,16 +6,17 @@ const storage = new  CloudinaryStorage({
   cloudinary,
   params: (req, file) => {
     let folderName = 'skillitgh';
+    const baseUrl = req.baseUrl || '';
 
-    if (req.baseUrl.includes('courses')) {
+    if (baseUrl.includes('courses')) {
       folderName = 'skillitgh/courses';
-    } else if (req.baseUrl.includes('workshops')) {
+    } else if (baseUrl.includes('workshops')) {
       folderName = 'skillitgh/workshops';
-    } else if (req.baseUrl.includes('users')) {
+    } else if (baseUrl.includes('users')) {
       folderName = 'skillitgh/profiles';
-    } else if (req.body.resource) {
-      folderName = `skillitgh/${req.body.resource}`;
-    }
+    } // else if (req.body.resource) {
+    //   folderName = `skillitgh/${req.body.resource}`;
+    // }
     return {
       folder: folderName,
       allowed_formats: ['jpg', 'png', 'jpeg', 'pdf', 'docx', 'pptx'],
@@ -27,13 +28,14 @@ const storage = new  CloudinaryStorage({
 const upload = multer({ storage });
 
 // Middleware to handle file upload
-// const uploadFile = (req, res, next) => {
-//   upload.single('courseImage')(req, res, (err) => {
-//     if (err) {
-//       return res.status(400).json({ error: 'File upload failed' });
-//     }
-//     next();
-//   });
-// };
+const uploadFile = (req, res, next) => {
+  upload.single('workshopImage')(req, res, (err) => {
+    if (err) {
+      console.error('Error uploading file:', err.message);
+      return res.status(400).json({ error: 'File upload failed' });
+    }
+    next();
+  });
+};
 
-module.exports = upload;
+module.exports = {upload, uploadFile};
