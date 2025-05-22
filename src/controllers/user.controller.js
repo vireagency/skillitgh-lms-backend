d08b176e1f0a475f6c06fd7e3c7a88bc2e29a090
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-22 17:18:15 +0000

    Refactor user profile update and delete functionality to integrate Cloudinary image handling

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.model');
+const cloudinary = require('../utils/cloudinaryHelper');
 
 exports.getUserProfile = async (req, res) => {
   try {
@@ -29,32 +30,43 @@ exports.updateUserProfile = async (req, res) => {
     if (!firstName || !lastName || !email) {
       return res.status(400).json({ success: false, message: "All fields are required!" });
     }
-    const updateUser = await User.findByIdAndUpdate(userId, {
-      firstName,
-      lastName,
-      email,
-      gender,
-      location,
-      phoneNumber,
-      userImage
-    }, { new: true, runValidators: true });
+    // const updateUser = await User.findByIdAndUpdate(userId, {
+    //   firstName,
+    //   lastName,
+    //   email,
+    //   gender,
+    //   location,
+    //   phoneNumber,
+    //   userImage
+    // }, { new: true, runValidators: true });
     
-    if (!updateUser) {
-      return res.status(400).json({ success: false, message: "User profile not updated!" });
-    }
-
-    // const user = await User.findById(userId);
-    // if (!user) {
-    //   return res.status(404).json({ success: false, message: "User not found!" });
-    // }
-    // user.firstName = firstName;
-    // user.lastName = lastName;
-    // user.email = email;
-    // const updateUser = await user.save();
-
     // if (!updateUser) {
     //   return res.status(400).json({ success: false, message: "User profile not updated!" });
     // }
+
+   const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User profile not updated!" });
+    }
+    if (userImage !== user.userImage && user.userImagePublicId) {
+      await cloudinary.deleteFromCloudinary(user.userImagePublicId);
+    }
+    user.userImage = userImage || user.userImage;
+    // user.userImagePublicId = userImage ? userImage.split('/').pop().split('.')[0] : user.userImagePublicId;
+
+    if (userImage && req.file?.filename) {
+      user.userImage = userImage;
+      user.userImagePublicId = req.file.filename;
+    }
+    user.firstName = firstName || user.firstName;
+    user.lastName = lastName || user.lastName;
+    user.email = email || user.email;
+    user.gender = gender || user.gender;
+    user.location = location || user.location;
+    user.phoneNumber = phoneNumber || user.phoneNumber;
+    user.role = req.body.role || user.role;
+
+    const updateUser = await user.save();
     res.status(200).json({ success: true, message: "User profile updated successfully!", user: updateUser });
   } catch (error) {
     console.error("Error editing user profile:", error);
@@ -68,10 +80,15 @@ exports.deleteUserProfile = async (req, res) => {
     if (!userId) {
       return res.status(401).json({ success: false, message: "Unauthorized: Please Login." });
     }
-    const user = await User.findByIdAndDelete(userId);
+    const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ success: false, message: "User not found!" });
     }
+    if (user.userImagePublicId) {
+      await cloudinary.deleteFromCloudinary(user.userImagePublicId);
+    }
+    await User.findByIdAndDelete(userId);
+
     res.status(200).json({ success: true, message: "User profile deleted successfully!" });
   } catch (error) {
     console.error("Error deleting user profile:", error);
@@ -116,10 +133,15 @@ exports.deleteUserProfileByAdmin = async (req, res) => {
     if (!userId) {
       return res.status(401).json({ success: false, message: "Unauthorized: Please Login." });
     }
-    const user = await User.findByIdAndDelete(userId);
+    const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ success: false, message: "User not found!" });
     }
+    if (user.userImagePublicId) {
+      await cloudinary.deleteFromCloudinary(user.userImagePublicId);
+    }
+    await User.findByIdAndDelete(userId);
+    
     res.status(200).json({ success: true, message: "User profile deleted successfully!" });
   } catch (error) {
     console.error("Error deleting user profile by admin:", error);
@@ -133,24 +155,44 @@ exports.updateUserProfileByAdmin = async (req, res) => {
     if (!userId) {
       return res.status(401).json({ success: false, message: "Unauthorized: Please Login." });
     }
-    const { firstName, lastName, email, gender, location, phoneNumber } = req.body;
+    const { firstName, lastName, email, gender, location, phoneNumber, role } = req.body;
     const userImage = req.file?.path;
     if (!firstName || !lastName || !email) {
       return res.status(400).json({ success: false, message: "All fields are required!" });
     }
-    const updateUser = await User.findByIdAndUpdate(userId, {
-      firstName,
-      lastName,
-      email,
-      gender,
-      location,
-      phoneNumber,
-      userImage
-    }, { new: true, runValidators: true }
-    );
-    if (!updateUser) {
-      return res.status(400).json({ success: false, message: "User profile not updated!" });
+    // const updateUser = await User.findByIdAndUpdate(userId, {
+    //   firstName,
+    //   lastName,
+    //   email,
+    //   gender,
+    //   location,
+    //   phoneNumber,
+    //   userImage
+    // }, { new: true, runValidators: true }
+    // );
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User profile not updated!" });
     }
+    if (userImage !== user.userImage && user.userImagePublicId) {
+      await cloudinary.deleteFromCloudinary(user.userImagePublicId);
+    }
+    user.userImage = userImage || user.userImage;
+    // user.userImagePublicId = userImage ? userImage.split('/').pop().split('.')[0] : user.userImagePublicId;
+
+    if (userImage && req.file?.filename) {
+      user.userImage = userImage;
+      user.userImagePublicId = req.file.filename;
+    }
+    user.firstName = firstName || user.firstName;
+    user.lastName = lastName || user.lastName;
+    user.email = email || user.email;
+    user.gender = gender || user.gender;
+    user.location = location || user.location;
+    user.phoneNumber = phoneNumber || user.phoneNumber;
+    user.role = role || user.role;
+
+    const updateUser = await user.save();
     res.status(200).json({ success: true, message: "User profile updated successfully!", user: updateUser });
   } catch (error) {
     console.error("Error updating user profile by admin:", error);
