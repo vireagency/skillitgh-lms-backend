00225adcae384f002e693410f386bf33ed091a52
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-05-22 17:20:56 +0000

    Add Cloudinary helper functions for image upload and deletion

---
+++ src/utils/cloudinaryHelper.js
@@ -0,0 +1,30 @@
+const cloudinary = require('../../config/cloudinary');
+
+exports.deleteFromCloudinary = async (publicId) => {
+  if (!publicId) {
+    try { 
+      await  cloudinary.uploader.destroy(publicId);
+      console.log(`Successfully deleted image with public ID: ${publicId}`);
+    } catch (error) {
+      console.error("Error deleting image from Cloudinary:", error);
+      throw new Error("Failed to delete image from Cloudinary");
+    }
+  }
+}
+
+exports.uploadToCloudinary = async (filePath) => {
+  if (!filePath) {
+    try {
+      const result = await cloudinary.uploader.upload(filePath, {
+        folder: 'workshop-images',
+        use_filename: true,
+        unique_filename: false,
+        overwrite: true
+      });
+      return result.secure_url;
+    } catch (error) {
+      console.error("Error uploading image to Cloudinary:", error);
+      throw new Error("Failed to upload image to Cloudinary");
+    }
+  }
+}
