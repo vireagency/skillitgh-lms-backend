5c818967d7610563e0cdacdb27912391722502a5
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-10-30 11:19:20 +0000

    Refactor: Simplify unique share ID generation

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,42 +1,10 @@
 const Workshop = require("../models/workshop.model");
-import("nanoid");
-//const { nanoid } = require("nanoid");
-
-exports.UniqueShareId = async function (length = 10) {
-  const array = new Uint8Array(length);
-  window.crypto.getRandomValues(array);
-  return Array.from(array, (byte) => ("0" + byte.toString(16)).slice(-2))
-    .join("")
-    .slice(0, length);
-};
-
-exports.generateHash = async (input) => {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(input);
-  const hashBuffer = await window.crypto.subtle.digest("SHA-256", data);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray
-    .map((b) => b.toString(16).padStart(2, "0"))
-    .join("");
-  return hashHex;
-};
-
-exports.generateShortHash = async (input, length = 10) => {
-  const fullHash = await generateHash(input);
-  return fullHash.slice(0, length);
-};
-
-exports.generateRandomHash = async (length = 10) => {
-  const array = new Uint8Array(length);
-  window.crypto.getRandomValues(array);
-  return Array.from(array, (byte) => ("0" + byte.toString(16)).slice(-2))
-    .join("")
-    .slice(0, length);
-};
 
 exports.generateUniqueShareId = async (length = 10) => {
   let id;
   let found = true;
+
+  const { nanoid } = await import("nanoid");
 
   while (found) {
     id = nanoid(length);
