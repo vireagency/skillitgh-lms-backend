f24e8d84b00487fbebe78de41d51aa1ecea39ecd
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-10-31 16:59:04 +0000

    Feat: Add route for user notifications.

--- src/routes/notification.route.js
+++ src/routes/notification.route.js
@@ -1,24 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { 
-  getAllNotifications, 
-  markNotificationAsRead, 
+const {
+  getAllNotifications,
+  markNotificationAsRead,
   deleteNotification,
   deleteAllNotificationsByAdmin,
+  markAllNotificationsAsReadByAdmin,
+  findNotificationsByUserId,
+} = require("../controllers/notification.controller");
+
+const { auth } = require("../middlewares/auth.middleware");
+const { authorizeRole } = require("../middlewares/role.middleware");
+
+router.get(
+  "/dashboard/notifications",
+  auth,
+  authorizeRole("admin"),
+  getAllNotifications
+);
+
+router.get("/dashboard/user/notifications", auth, findNotificationsByUserId);
+
+router.put(
+  "/dashboard/notifications/:notificationId",
+  auth,
+  authorizeRole("admin"),
+  markNotificationAsRead
+);
+
+router.delete(
+  "/dashboard/notifications/:notificationId",
+  auth,
+  authorizeRole("admin"),
+  deleteNotification
+);
+
+router.delete(
+  "/dashboard/notifications",
+  auth,
+  authorizeRole("admin"),
+  deleteAllNotificationsByAdmin
+);
+
+router.put(
+  "/dashboard/notifications",
+  auth,
+  authorizeRole("admin"),
   markAllNotificationsAsReadByAdmin
-} = require('../controllers/notification.controller');
-
-const { auth } = require('../middlewares/auth.middleware');
-const { authorizeRole } = require('../middlewares/role.middleware');
-
-router.get('/dashboard/notifications', auth, authorizeRole('admin'), getAllNotifications);
-
-router.put('/dashboard/notifications/:notificationId', auth, authorizeRole('admin'), markNotificationAsRead);
-
-router.delete('/dashboard/notifications/:notificationId', auth, authorizeRole('admin'), deleteNotification);
-
-router.delete('/dashboard/notifications', auth, authorizeRole('admin'), deleteAllNotificationsByAdmin);
-
-router.put('/dashboard/notifications', auth, authorizeRole('admin'), markAllNotificationsAsReadByAdmin);
+);
 
 module.exports = router;
