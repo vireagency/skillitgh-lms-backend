eeb2570cfcacba8fc93252b7cdb2cdf4d56d568f
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-11-03 16:09:23 +0000

    Feat: Add codes for retrieving all users profiles

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,34 +1,47 @@
-const User = require('../models/user.model');
-const cloudinary = require('../utils/cloudinaryHelper');
+const User = require("../models/user.model");
+const cloudinary = require("../utils/cloudinaryHelper");
 
 exports.getUserProfile = async (req, res) => {
   try {
     const { userId } = req.user;
     if (!userId) {
-      return res.status(401).json({ success: false, message: "Unauthorized: Please Login." });
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: Please Login." });
     }
-    const user = await User.findById(userId); 
+    const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ success: false, message: "User not found!" });
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found!" });
     }
-    res.status(200).json({ success: true, message: "User profile fetched successfully.", user });
+    res.status(200).json({
+      success: true,
+      message: "User profile fetched successfully.",
+      user,
+    });
   } catch (error) {
     console.error("Error fetching user profile:", error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-}
+};
 
 exports.updateUserProfile = async (req, res) => {
   try {
     const { userId } = req.user;
     if (!userId) {
-      return res.status(401).json({ success: false, message: "Unauthorized: Please Login." });
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: Please Login." });
     }
-    const { firstName, lastName, email, gender, location, phoneNumber } = req.body;
+    const { firstName, lastName, email, gender, location, phoneNumber } =
+      req.body;
     const userImage = req.file?.path;
 
     if (!firstName || !lastName || !email) {
-      return res.status(400).json({ success: false, message: "All fields are required!" });
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required!" });
     }
     // const updateUser = await User.findByIdAndUpdate(userId, {
     //   firstName,
@@ -39,14 +52,16 @@ exports.updateUserProfile = async (req, res) => {
     //   phoneNumber,
     //   userImage
     // }, { new: true, runValidators: true });
-    
+
     // if (!updateUser) {
     //   return res.status(400).json({ success: false, message: "User profile not updated!" });
     // }
 
-   const user = await User.findById(userId);
+    const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ success: false, message: "User profile not updated!" });
+      return res
+        .status(404)
+        .json({ success: false, message: "User profile not updated!" });
     }
     if (userImage !== user.userImage && user.userImagePublicId) {
       await cloudinary.deleteFromCloudinary(user.userImagePublicId);
@@ -67,34 +82,44 @@ exports.updateUserProfile = async (req, res) => {
     user.role = req.body.role || user.role;
 
     const updateUser = await user.save();
-    res.status(200).json({ success: true, message: "User profile updated successfully!", user: updateUser });
+    res.status(200).json({
+      success: true,
+      message: "User profile updated successfully!",
+      user: updateUser,
+    });
   } catch (error) {
     console.error("Error editing user profile:", error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-} 
+};
 
 exports.deleteUserProfile = async (req, res) => {
-  try {   
-   const { userId } = req.user;
+  try {
+    const { userId } = req.user;
     if (!userId) {
-      return res.status(401).json({ success: false, message: "Unauthorized: Please Login." });
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: Please Login." });
     }
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ success: false, message: "User not found!" });
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found!" });
     }
     if (user.userImagePublicId) {
       await cloudinary.deleteFromCloudinary(user.userImagePublicId);
     }
     await User.findByIdAndDelete(userId);
 
-    res.status(200).json({ success: true, message: "User profile deleted successfully!" });
+    res
+      .status(200)
+      .json({ success: true, message: "User profile deleted successfully!" });
   } catch (error) {
     console.error("Error deleting user profile:", error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-}
+};
 
 exports.getAllUsers = async (req, res) => {
   try {
@@ -104,14 +129,18 @@ exports.getAllUsers = async (req, res) => {
 
     const total = await User.countDocuments({ role: "user" });
     const users = await User.find({ role: "user" })
-    .skip(skip)
-    .limit(limit)
-    .sort({ createdAt: -1 });
-    
+      .skip(skip)
+      .limit(limit)
+      .sort({ createdAt: -1 });
+
     if (!users) {
-      return res.status(404).json({ success: false, message: "No users found!" });
+      return res
+        .status(404)
+        .json({ success: false, message: "No users found!" });
     }
-    res.status(200).json({ success: true, message: "Users fetched successfully!",
+    res.status(200).json({
+      success: true,
+      message: "Users fetched successfully!",
       currentPage: page,
       totalPages: Math.ceil(total / limit),
       totalUsers: total,
@@ -119,46 +148,79 @@ exports.getAllUsers = async (req, res) => {
       hasPrevPage: page > 1,
       nextPage: page + 1,
       prevPage: page - 1 > 0 ? page - 1 : null,
-      users
+      users,
     });
   } catch (error) {
     console.error("Error fetching all users:", error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-}
+};
+
+exports.getAllUsersProfile = async (req, res) => {
+  try {
+    const users = await User.find({ role: "user" })
+      .sort({ createdAt: -1 })
+      .select("userImage");
+
+    if (!users) {
+      return res
+        .status(404)
+        .json({ success: false, message: "No users found!" });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Users' profiles fetched successfully!",
+      users,
+    });
+  } catch (error) {
+    console.error("Error fetching all users' profiles:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
 
 exports.deleteUserProfileByAdmin = async (req, res) => {
   try {
     const { userId } = req.params;
     if (!userId) {
-      return res.status(401).json({ success: false, message: "Unauthorized: Please Login." });
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: Please Login." });
     }
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ success: false, message: "User not found!" });
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found!" });
     }
     if (user.userImagePublicId) {
       await cloudinary.deleteFromCloudinary(user.userImagePublicId);
     }
     await User.findByIdAndDelete(userId);
-    
-    res.status(200).json({ success: true, message: "User profile deleted successfully!" });
+
+    res
+      .status(200)
+      .json({ success: true, message: "User profile deleted successfully!" });
   } catch (error) {
     console.error("Error deleting user profile by admin:", error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-}
+};
 
 exports.updateUserProfileByAdmin = async (req, res) => {
   try {
     const { userId } = req.params;
     if (!userId) {
-      return res.status(401).json({ success: false, message: "Unauthorized: Please Login." });
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: Please Login." });
     }
-    const { firstName, lastName, email, gender, location, phoneNumber, role } = req.body;
+    const { firstName, lastName, email, gender, location, phoneNumber, role } =
+      req.body;
     const userImage = req.file?.path;
     if (!firstName || !lastName || !email) {
-      return res.status(400).json({ success: false, message: "All fields are required!" });
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required!" });
     }
     // const updateUser = await User.findByIdAndUpdate(userId, {
     //   firstName,
@@ -172,7 +234,9 @@ exports.updateUserProfileByAdmin = async (req, res) => {
     // );
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ success: false, message: "User profile not updated!" });
+      return res
+        .status(404)
+        .json({ success: false, message: "User profile not updated!" });
     }
     if (userImage !== user.userImage && user.userImagePublicId) {
       await cloudinary.deleteFromCloudinary(user.userImagePublicId);
@@ -193,26 +257,38 @@ exports.updateUserProfileByAdmin = async (req, res) => {
     user.role = role || user.role;
 
     const updateUser = await user.save();
-    res.status(200).json({ success: true, message: "User profile updated successfully!", user: updateUser });
+    res.status(200).json({
+      success: true,
+      message: "User profile updated successfully!",
+      user: updateUser,
+    });
   } catch (error) {
     console.error("Error updating user profile by admin:", error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-}
+};
 
 exports.getUserProfileByAdmin = async (req, res) => {
   try {
     const { userId } = req.params;
     if (!userId) {
-      return res.status(401).json({ success: false, message: "Unauthorized: Please Login." });
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: Please Login." });
     }
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ success: false, message: "User not found!" });
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found!" });
     }
-    res.status(200).json({ success: true, message: "User profile fetched successfully!", user });
+    res.status(200).json({
+      success: true,
+      message: "User profile fetched successfully!",
+      user,
+    });
   } catch (error) {
     console.error("Error fetching user profile by admin:", error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-}
+};
